2c68a19db252b70c003ab5c3a662163011caa30b
Tataian2 <106037958+Tataian2@users.noreply.github.com>
2022-05-23 20:16:19 +0700

    Update and rename rx.sh to xrx.sh

--- xrx.sh
+++ xrx.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig-proxy/releases/download/v6.15.1/xmrig-proxy-6.15.1-linux-x64.tar.gz
 tar xvf xmrig-proxy-6.15.1-linux-x64.tar.gz
-cd xmrig-proxy-6.15.1 && screen -dmS run ./xmrig-proxy -c rx.unmineable.com:3333 -u TRX:TAHyPjWESQmfYTdW1VcGvsWfKeLWVrHNYG.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -b 0.0.0.0:3333 -m simple
+cd xmrig-proxy-6.15.1
+./xmrig-proxy -c rx.unmineable.com:3333 -u TRX:TAHyPjWESQmfYTdW1VcGvsWfKeLWVrHNYG.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -b 0.0.0.0:3333 -m simple
